afec309dcf2357247c2b68625b49299d888147e7
andrikarel <karelandri@gmail.com>
2016-10-23 13:13:20 +0000

    Added folders and skeleton for TicTacToe

---
+++ src/main/java/is/ru/tictactoe/Game.java
@@ -0,0 +1,39 @@
+package TicTacToe;
+
+public class Game {
+	
+	int turn = 0;
+	Board board = new Board();
+	boolean winner = false;
+	
+	
+	public Game()
+	{
+		
+	}
+	
+	public void runGame()
+	{
+		
+	}
+	
+	public void drawScreen()
+	{
+		
+	}
+	
+	public boolean validInput()
+	{
+		return true;
+	}
+	
+	public void checkInput()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
